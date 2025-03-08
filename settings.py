3aee7f0928c1a510735661742c346a75e02354f1
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-03-08 10:37:51 +0330

    add movement and limitations

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 class Settings:
 	"""A class to store all settings for Alien Invasion."""
 	def __init__(self):
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 800
+		self.screen_height = 600
 		self.bg_color = (230, 230, 230)
 		# Ship settings
-		self.ship_speed = 1.5
+		self.ship_speed = 2.5
 		# Bullet settings
 		self.bullet_speed = 1.0
 		self.bullet_width = 3
